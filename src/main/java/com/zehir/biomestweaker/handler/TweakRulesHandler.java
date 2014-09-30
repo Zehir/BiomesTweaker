7e0ce63c5fbe21be88275a5faff10932fba28bd0
Pierre-Baptiste Varlet <pierre-baptiste.varlet@univ-reims.fr>
2014-09-30 12:32:52 +0200

    Remove useless class

--- src/main/java/com/zehir/biomestweaker/handler/TweakRulesHandler.java
+++
@@ -1,9 +0,0 @@
-package com.zehir.biomestweaker.handler;
-
-import com.zehir.biomestweaker.utility.TweakRule;
-
-import cpw.mods.fml.relauncher.ReflectionHelper;
-
-public class TweakRulesHandler {
-	public TweakRule[]	tweakRules	= null;
-}
